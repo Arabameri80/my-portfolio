833567cddcd87ef7de2db3228486b8f6acd2df1a
Amirhossein Arabameri <a.h.arabameri@gmail.com>
2026-06-23 13:04:02 +0330

    initial commit

---
+++ public/app.js
@@ -0,0 +1,6 @@
+const menu_icon = document.getElementById("menu-icon");
+const icon = document.querySelector(".hambergur-icon");
+
+menu_icon.addEventListener("click", function () {
+  icon.classList.toggle("x-icon");
+});
