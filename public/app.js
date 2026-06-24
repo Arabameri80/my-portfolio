f8fdd2705578b24bebea2d96a4a501850580cdeb
Amirhossein Arabameri <a.h.arabameri@gmail.com>
2026-06-24 18:26:15 +0330

    Complete responsive UI for header and main section

--- public/app.js
+++ public/app.js
@@ -1,6 +1,23 @@
 const menu_icon = document.getElementById("menu-icon");
 const icon = document.querySelector(".hambergur-icon");
+const mobile_menu = document.querySelector(".mobile_menu");
+const overlay = document.querySelector(".overlay");
+const nav = document.querySelector(".nav");
 
 menu_icon.addEventListener("click", function () {
   icon.classList.toggle("x-icon");
+  mobile_menu.classList.toggle("mobile_menu-open");
+  overlay.classList.toggle("overlay-open");
 });
+
+overlay.addEventListener("click", function () {
+  icon.classList.toggle("x-icon");
+  mobile_menu.classList.toggle("mobile_menu-open");
+  overlay.classList.toggle("overlay-open");
+});
+
+// nav.addEventListener("click", function () {
+//   icon.classList.remove("x-icon");
+//   mobile_menu.classList.remove("mobile_menu-open");
+//   overlay.classList.remove("overlay-open");
+// });
