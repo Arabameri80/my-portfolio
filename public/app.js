dfa8817e9685e426266f84b258fbc3d4a8fce197
Amirhossein Arabameri <a.h.arabameri@gmail.com>
2026-06-28 18:03:32 +0330

    resume section completed.

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ const icon = document.querySelector(".hambergur-icon");
 const mobile_menu = document.querySelector(".mobile_menu");
 const overlay = document.querySelector(".overlay");
 const nav = document.querySelector(".nav");
+const items = document.querySelectorAll(".item");
 
 menu_icon.addEventListener("click", function () {
   icon.classList.toggle("x-icon");
@@ -16,8 +17,20 @@ overlay.addEventListener("click", function () {
   overlay.classList.toggle("overlay-open");
 });
 
-// nav.addEventListener("click", function () {
-//   icon.classList.remove("x-icon");
-//   mobile_menu.classList.remove("mobile_menu-open");
-//   overlay.classList.remove("overlay-open");
-// });
+items.forEach(function (item) {
+  item.addEventListener("click", function () {
+    items.forEach(function (otherItem) {
+      const linkBtn = otherItem.querySelector(".link_btn");
+      const colorSpan = otherItem.querySelector(".item_color");
+
+      linkBtn.classList.remove("selected_item");
+      colorSpan.classList.remove("text-white");
+    });
+
+    const currentLinkBtn = this.querySelector(".link_btn");
+    const currentColorSpan = this.querySelector(".item_color");
+
+    currentLinkBtn.classList.add("selected_item");
+    currentColorSpan.classList.add("text-white");
+  });
+});
